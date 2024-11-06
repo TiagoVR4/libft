9bc187565792eea8b00891e98c070b5abed0976d
TiagoVR4 <79290705+TiagoVR4@users.noreply.github.com>
2024-11-06 12:56:37 +0000

    Update ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -20,11 +20,11 @@ char	*ft_strrchr(const char *str, int c)
 	last_c = NULL;
 	while (*str)
 	{
-		if (*str == (char)c)
+		if (*str == (unsigned char)c)
 			last_c = (char *)str;
 		str++;
 	}
-	if (c == '\0')
+	if ((unsigned char)c == '\0')
 		return ((char *)str);
 	return (last_c);
 }
