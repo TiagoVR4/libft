0158bb42706f4aad1229ba6382874ceb9b21c202
TiagoVR4 <79290705+TiagoVR4@users.noreply.github.com>
2024-11-06 12:55:30 +0000

    Update ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -36,36 +36,39 @@ size_t	ft_word_counter(char *s, char c)
 	return (count);
 }
 
-//Divide uma str em uma array de str's c/base de char escolhido pelo utilizador.
-char	**ft_split(const char *s, char c)
+void	ft_make_array(char *s, char c, char **result)
 {
 	size_t	i;
 	size_t	start;
 	size_t	index;
-	char	**result;
 
 	i = 0;
 	start = 0;
 	index = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] != c && (i == 0 || s[i - 1] == c))
+			start = i;
+		if (s[i] == c && i > 0 && (s[i - 1] != c))
+			result[index++] = ft_substr(s, start, i - start);
+		i++;
+	}
+	if (i > 0 && s[i - 1] != c)
+		result[index++] = ft_substr(s, start, i - start);
+	result[index] = NULL;
+}
+
+//Divide uma str em uma array de str's c/base de char escolhido pelo utilizador.
+char	**ft_split(const char *s, char c)
+{
+	char	**result;
+
 	if (!s)
 		return (NULL);
 	result = malloc((ft_word_counter((char *)s, c) + 1) * sizeof(char *));
 	if (!result)
 		return (NULL);
-	while (s[i] != '\0')
-	{
-		if (s[i] != c && start == i)
-			start = i;
-		else if (s[i] == c && start != i)
-		{
-			result[index++] = ft_substr(s, start, i - start);
-			start = i + 1;
-		}
-		i++;
-	}
-	if (start < i)
-		result[index++] = ft_substr(s, start, i - start);
-	result[index] = NULL;
+	ft_make_array((char *)s, c, result);
 	return (result);
 }
 /* 
@@ -81,5 +84,6 @@ int	main()
 		printf("%s\n", result[i]);
 		i++;
 	}
+	free(result);
 	return (0);
 } */
